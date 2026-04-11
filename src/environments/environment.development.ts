cb7fe5713e99646a613cd3b7013c8e162e550d0d
Joseph torres <josephnicolastorres6@gmail.com>
2026-04-10 21:48:08 -0500

    Actualizando URL de producción

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://celus-papu-comunidad-production.up.railway.app'
+// Prueba dejándolo así:
+apiUrl: 'https://celus-papu-comunidad-production.up.railway.app/api'
 };
