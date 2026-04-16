7ae82fc2b458cf8252400071b6a93f2cd2ead085
Joseph torres <josephnicolastorres6@gmail.com>
2026-04-16 14:36:26 -0500

    Fix: Configuración de CORS para Vercel

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const app = express();
 // CONFIGURACIÓN DE CORS (Requerimiento B.1)
 // ==========================================
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    origin: [process.env.FRONTEND_URL, 'https://celus-papu-comunidad-633mozfza.vercel.app', 'http://localhost:4200'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'] 
 };
 
